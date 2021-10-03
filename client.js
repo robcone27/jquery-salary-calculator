c8d9ebe13fe8260cb9a48a2db75d37ade44fb9c9
Rob Cone <robcone86@gmail.com>
2021-10-03 11:33:51 -0500

    repo update 10.3

--- client.js
+++ client.js
@@ -1,22 +1,65 @@
 console.log('js')
-
+let addEmployeeList = [];
 $(readyNow);
 
-function readyNow(){
-   $(`#enterField`).on(`click`, addInfo) 
+function readyNow() {
+    $(`#enterField`).on(`click`, addInfo);
+    $(`#targeted`).on(`click`, `.deleteEmployee`, deleteEmployee);
 }
-function addInfo(){
-    console.log('it works!')
+function addInfo() {
+    console.log('it works!');
 
 
-const employeeInfo = {
-    firstName: $(`#firstName`).val(),
-    lastName: $(`#lastName`).val(),
-    idNumber: $(`#idNumber`).val(),
-    jobTitle: $(`#jobTitle`).val(),
-    annualSalary: $(`#annualSalary`).val(),
-}
-console.log(employeeInfo)
+    const addEmployeeInfo = {
+        firstName: $(`#firstName`).val(),
+        lastName: $(`#lastName`).val(),
+        idNumber: $(`#idNumber`).val(),
+        jobTitle: $(`#jobTitle`).val(),
+        annualSalary: $(`#annualSalary`).val(),
+    }
+    console.log(addEmployeeInfo);
 
+    addEmployeeList.push(addEmployeeInfo);
+
+    renderInfo();
+    // $(`#targeted`).append(row)
 }
-// employee first name, last name, ID number, job title, annual salary_.
+
+function renderInfo() {
+    $(`#targeted`).empty();
+
+    for (let addEmployeeInfo of addEmployeeList) {
+        console.log(addEmployeeInfo.firstName1)
+        const row = $(`
+     <tr>
+  <td>${addEmployeeInfo.firstName}</td>
+  <td>${addEmployeeInfo.lastName}</td>
+  <td>${addEmployeeInfo.idNumber}</td>
+  <td>${addEmployeeInfo.jobTitle}</td>
+  <td>${addEmployeeInfo.annualSalary}</td>
+  <td><button class="deleteEmployee">Delete</button></td>
+</tr>
+`);
+
+        $(`#targeted`).append(row);
+    }
+};
+
+
+function monthlyTotal() {
+    let totalSalary = 0;
+    for (let i = 0; i < addEmployeeList.length; i++) {
+        let employeeSalary = Number(addEmployeeList[i].annualSalary);
+        totalSalary += totalMonthly
+    }
+    totalSalary += employeeSalary / 12;
+
+    $(`#totalMonthly`).append(totalSalary);
+
+};
+
+function deleteEmployee() {
+    console.log('deleted')
+    $(this).parent().parent().remove();
+    $(this).closest('tr').remove();
+};
